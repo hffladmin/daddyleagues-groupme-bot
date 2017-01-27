d2d8e8a263b80bde3ddd93e43db68aa3df8d24ab
hffladmin <tetrarc@live.com>
2017-01-26 22:32:06 -0500

    Update mainBot.php

--- mainBot.php
+++ mainBot.php
@@ -6,7 +6,7 @@ include('./httpful.phar');
 include('./functions.php');
 
 // this is found at http://dev.groupme.com/bots
-$bot_token = "17c717855583810fc795df353b";
+$bot_token = "f39d6c189a4d432c1b24cf0dbd";
 
 include('./index.php');
 
